d6b1cce44cb63457af41f7681ad2d1bf9012bc37
fabbrik <fabriz@Mac.lan>
2025-03-04 14:11:07 -0600

    added await to async test method

--- tests/Testcontainers.Spanner.Tests/SpannerContainerTest.cs
+++ tests/Testcontainers.Spanner.Tests/SpannerContainerTest.cs
@@ -29,6 +29,6 @@ public sealed class SpannerContainerTest : IAsyncLifetime
         using var connection = new SpannerConnection(connectionString);
 
         // Then
-        connection.Open();
+        await connection.OpenAsync();
     }
 }
